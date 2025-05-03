caf5b12adb22b37ee584949f64cd32f03f86f8de
Iago Ferreira <iago-ferreira@outlook.com>
2025-05-02 22:52:20 -0300

    feat: added healthcheck handler

---
+++ leaky-bucket/server/internal/http/health_check_handler.go
@@ -0,0 +1,9 @@
+package internal
+
+import "net/http"
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status": "healthy"}`))
+}
